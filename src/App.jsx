fcd29adb71230a9bbb18f0fc472801d5e794fa4a
Tanya <taweder@gmail.com>
2025-11-21 06:21:17 -0700

    Variable Change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  const name = "Your Mom";
+  const name = "Programming Class";
 
   return (
     <>
